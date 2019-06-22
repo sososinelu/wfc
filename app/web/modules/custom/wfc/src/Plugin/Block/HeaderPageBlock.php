04e9ebebea5abb3880fbd5f381bbd8eb7d3430b7
Sorin Secan <ssecan@grademaker.com>
2019-06-22 18:38:12 +0100

    Footer, FAQs listing and general page re-design

--- app/web/modules/custom/wfc/src/Plugin/Block/HeaderPageBlock.php
+++ app/web/modules/custom/wfc/src/Plugin/Block/HeaderPageBlock.php
@@ -1,10 +1,10 @@
 <?php
 /**
- * Provides the header sign up block
+ * Provides the header page block
  *
  * @Block(
- *   id = "header_sign_up_block",
- *   admin_label = @Translation("Header sign up block"),
+ *   id = "header_page_block",
+ *   admin_label = @Translation("Header page block"),
  * )
  */
 
@@ -12,7 +12,7 @@ namespace Drupal\wfc\Plugin\Block;
 use Drupal\block\Entity\Block;
 use Drupal\Core\Block\BlockBase;
 
-class HeaderSignUpBlock extends BlockBase
+class HeaderPageBlock extends BlockBase
 {
   /**
    * @return array
@@ -22,14 +22,12 @@ class HeaderSignUpBlock extends BlockBase
     if($node = \Drupal::routeMatch()->getParameter('node')) {
       $slogan = (\Drupal::state()->get('site_slogan') ? \Drupal::state()->get('site_slogan')['value'] : '');
       $sign_up_text = (\Drupal::state()->get('sign_up_text') ? \Drupal::state()->get('sign_up_text')['value'] : '');
-      $signup_form = \Drupal::formBuilder()->getForm('Drupal\wfc_sendgrid\Form\SendGridEmailRegistrationForm');
 
       return array(
-        '#theme' => 'header_sign_up_template',
+        '#theme' => 'header_page_template',
         '#vars' => array(
           'slogan' => $slogan,
-          'sign_up_text' => $sign_up_text,
-          'sign_up_form' => $signup_form,
+          'sign_up_text' => $sign_up_text
         ),
         '#cache' => array('max-age' => 0),
       );
