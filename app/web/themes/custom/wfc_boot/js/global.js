06a738addfae0c2970a8356841338dfc5973093a
Sorin Secan <ssecan@grademaker.com>
2019-07-22 19:45:08 +0100

    Added the stripe form to the premium page

--- app/web/themes/custom/wfc_boot/js/global.js
+++ app/web/themes/custom/wfc_boot/js/global.js
@@ -1,60 +1,30 @@
-
-
+/**
+ * Premium page call to actions
+ * stripe form
+ */
 (function($) {
   $(document).ready(function() {
-    var notification = $('.overlay');
-    var close_button = $('.overlay-close');
-
-    $('#block-homepagehowitworksblock .panel span').click(function() {
-      notification.show();
-      //centrePopup();
-
-      $(document.body).addClass('noscroll');
-      notification.velocity({opacity: 1});
-      notification.scrollTop = 0;
+    $(".1-cta").click(function(){
+      $('.stripe-forms #quarterly .stripe-button-el').click();
     });
 
-    $(window).resize(function() {
-      //centrePopup();
+    $(".2-cta").click(function(){
+      $('.stripe-forms #semiannual .stripe-button-el').click();
     });
 
-    // $(document).click(function(event) {
-    //   if(!$(event.target).closest('.notification-inner').length) {
-    //     hidePopup();
-    //   }
-    // });
-
-    close_button.click(function(event) {
-      hidePopup();
-      $(document.body).removeClass('noscroll');
+    $(".3-cta").click(function(){
+      $('.stripe-forms #annual .stripe-button-el').click();
     });
-
-    function centrePopup() {
-      var popupHeight = notification.height();
-      var marginTop = popupHeight / 2;
-      notification.css('margin-top', '-' + marginTop + 'px');
-    }
-
-    function hidePopup() {
-      if(notification.is(":visible")) {
-        notification.velocity({opacity: 0}, 400, function(){
-          notification.hide();
-        });
-      }
-    }
-
   });
 })(jQuery);
-/*
- * form styling
+
+/**
+ * Form styling
  */
 (function ($) {
   $(document).ready(function() {
-
-    //equalizeHeights($('#block-homepagepremiumblock  .panel'));
-
     // Add placeholder to signup form
-    $('.form-email').attr('placeholder', 'I want cheap flights!');
+    // $('.form-email').attr('placeholder', 'I want cheap flights!');
 
     $('input.error, select.error').each(function() {
       $(this).parents('.form-item').first().addClass('error');
@@ -70,46 +40,9 @@
   });
 }(jQuery));
 
-
-
-/*
- * Equalise heights
+/**
+ * Scroll to the top
  */
-function equalizeHeights(selector) {
-  var heights = new Array();
-
-
-  // Loop to get all element heights
-  jQuery(selector).each(function() {
-
-    // Need to let sizes be whatever they want so no overflow on resize
-    jQuery(this).css('min-height', '0');
-    jQuery(this).css('max-height', 'none');
-    jQuery(this).css('height', 'auto');
-
-    // Then add size (no units) to array
-    heights.push(jQuery(this).outerHeight());
-  });
-
-  // Find max height of all elements
-  var max = Math.max.apply( Math, heights );
-
-  // Set all heights to max height
-  jQuery(selector).each(function() {
-    jQuery(this).css('height', max + 'px');
-  });
-}
-
-// call equalizeHeights() when using ajax
-(function($) {
-  Drupal.behaviors.equalizeHeights = {
-    attach: function (context, settings) {
-      equalizeHeights();
-    }
-  };
-})(jQuery);
-
-// scroll to the top
 (function($) {
   // When the user scrolls down 20px from the top of the document, show the button
   window.onscroll = function() {scrollFunction()};
@@ -123,7 +56,9 @@ function equalizeHeights(selector) {
   }
 
 
-// Select all links with hashes
+/**
+ * Select all links with hashes
+ */
 $('a[href*="#"]')
   .click(function(event) {
 
@@ -154,16 +89,11 @@ $('a[href*="#"]')
   })
 })(jQuery);
 
-
 /**
- * @file
  * Global utilities.
- *
  */
 (function($, Drupal) {
-
   'use strict';
-
   Drupal.behaviors.bootstrap_barrio_subtheme = {
     attach: function(context, settings) {
       var position = $(window).scrollTop();
@@ -184,8 +114,6 @@ $('a[href*="#"]')
         }
         position = scroll;
       });
-
     }
   };
-
 })(jQuery, Drupal);
