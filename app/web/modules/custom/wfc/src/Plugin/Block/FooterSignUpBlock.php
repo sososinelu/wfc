3c8212dce4ffefed31e276c87b6812d1e3c8813f
Sorin <sorinsoso4@gmail.com>
2019-08-15 09:35:17 +0100

    Removed node check from footer signup block

--- app/web/modules/custom/wfc/src/Plugin/Block/FooterSignUpBlock.php
+++ app/web/modules/custom/wfc/src/Plugin/Block/FooterSignUpBlock.php
@@ -19,20 +19,18 @@ class FooterSignUpBlock extends BlockBase
    */
   public function build()
   {
-    if($node = \Drupal::routeMatch()->getParameter('node')) {
-      $footer_sign_up_text = (\Drupal::state()->get('footer_sign_up_text') ? \Drupal::state()->get('footer_sign_up_text')['value'] : '');
-      $sign_up_text = (\Drupal::state()->get('sign_up_text') ? \Drupal::state()->get('sign_up_text')['value'] : '');
-      $signup_form = \Drupal::formBuilder()->getForm('Drupal\wfc_sendgrid\Form\FooterSendGridEmailRegistrationForm');
+    $footer_sign_up_text = (\Drupal::state()->get('footer_sign_up_text') ? \Drupal::state()->get('footer_sign_up_text')['value'] : '');
+    $sign_up_text = (\Drupal::state()->get('sign_up_text') ? \Drupal::state()->get('sign_up_text')['value'] : '');
+    $signup_form = \Drupal::formBuilder()->getForm('Drupal\wfc_sendgrid\Form\FooterSendGridEmailRegistrationForm');
 
-      return array(
-        '#theme' => 'footer_sign_up_template',
-        '#vars' => array(
-          'footer_sign_up_text' => $footer_sign_up_text,
-          'sign_up_form' => $signup_form,
-        ),
-        '#cache' => array('max-age' => 0),
-      );
-    }
+    return array(
+      '#theme' => 'footer_sign_up_template',
+      '#vars' => array(
+        'footer_sign_up_text' => $footer_sign_up_text,
+        'sign_up_form' => $signup_form,
+      ),
+      '#cache' => array('max-age' => 0),
+    );
 
     return array();
   }
